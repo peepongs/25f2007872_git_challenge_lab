59c59f9ba19791d0540c87766d199505b9aa523f
Preethy <25f2007872@ds.study.iitm.ac.in>
2026-01-21 16:44:01 -0800

    Starting a main.py file from the main branch

---
+++ main.py
@@ -0,0 +1,20 @@
+# this is a Python code to do simple arithmetic functions like SUM, DIFFERENCE, PRODUCT, QUOTIENT and POWER
+def Sum(a, b):
+    #write the code here
+
+
+def Difference (a, b):
+    #write the code here. Code should be for absolute difference
+
+
+def Product (a, b):
+    #write the code here.
+
+
+def Quotient (a, b):
+    # write the code here. 
+
+
+def Power (a, b):
+    # write the code here to return a to the power b.
+    
